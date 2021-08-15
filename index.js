7e9d0ee32626fa78ef511b32a063835dc098d5de
VelichkoAlexander <alvi@mail.ua>
2021-08-15 10:26:05 +0300

    fix: path name for Linux server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const {bot} = require('./src/Bot');
 const botCommands = require('./src/BotCommandsList');
 const botCommandsListeners = require('./src/BotCommandsListeners');
-const keyboardListener = require('./src/keyboardListener')
+const keyboardListener = require('./src/KeyboardListener')
 
 function start() {
   bot.setMyCommands(botCommands)
