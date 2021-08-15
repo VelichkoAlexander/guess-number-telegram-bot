9d6a8e8cf1d8c458cc1de8e070bd4756ad8b85cd
VelichkoAlexander <alvi@mail.ua>
2021-08-15 17:14:19 +0300

    add: error when bot token is missing

--- src/BotInit.js
+++ src/BotInit.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const token = process.env.TELEGRAM_TOKEN ? process.env.TELEGRAM_TOKEN : '';
 const TelegramAPI = require('node-telegram-bot-api');
 if (!token) {
-  return;
+  throw new Error('Please provide a telegram bot token.');
 }
 const chats = {};
 const botInit = new TelegramAPI(token, {polling: true});
